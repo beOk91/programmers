4cc04e49b3d190f2d1e10855225af9eb053b8adf
beOk91 <be_ok91@naver.com>
2023-02-11 09:06:31 +0900

    add problem120844

---
+++ level0/Problem120844.java
@@ -0,0 +1,23 @@
+package level0;
+
+public class Problem120844 {
+	public static void main(String[] args) {
+
+	}
+	
+	public int[] solution(int[] numbers, String direction) {
+        int[] answer = new int[numbers.length];
+        if(direction.equals("right")){
+            for(int i=1;i<numbers.length;i++){
+                answer[i]=numbers[i-1];
+            }
+            answer[0]=numbers[numbers.length-1];
+        }else{
+            for(int i=1;i<numbers.length;i++){
+                answer[i-1]=numbers[i];
+            }
+            answer[numbers.length-1]=numbers[0];
+        }
+        return answer;
+    }
+}
